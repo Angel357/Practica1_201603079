e9dbfd8089a4b769a202a1414b054528d8b640e0
Angel <Angel@192.168.1.10>
2017-08-19 00:17:44 -0600

    ya se puede interactuar con el juego, descubriendo casillas si no son bombas ademas de poder perder y reiniciar el juego si se intenta descubrir una bomba

--- src/practica1/Matriz.java
+++ src/practica1/Matriz.java
@@ -1,15 +1,22 @@
 
 package practica1;
 import java.util.Random;
+import java.util.Scanner;
 public class Matriz {
     // objetos y variables globales
     Random ram=new Random();
     Random ram2=new Random();
-    int opcionMenu, fil,col, numBomba, filAL,colAL;
+    Scanner scan=new Scanner(System.in);
+    int opcionMenu, fil,col, numBomba, filAL,colAL,opJuego,selFil,selCol;
     int interna[][]; 
+    String Externa[][];
     //hh
     public void Dificultad(int opcionMenu){
+        try{
         this.opcionMenu=opcionMenu;
+        }catch(Exception sel){
+            
+        }
         switch(opcionMenu){
             case 1:
                 fil=4;
@@ -58,8 +65,7 @@ public class Matriz {
            interna[filAL][colAL]=-1;}       
        }// for de minas 
        //Esta parte es para el llenado de toda la matriz en donde no hayan minas 
-       for(int x=0;x<fil;x++){
-               System.out.println("");          
+       for(int x=0;x<fil;x++){      
            for(int y=0;y<col;y++){
                if(interna[x][y]!=-1){
                    int contMina=0;
@@ -111,8 +117,91 @@ public class Matriz {
                    }//for
                interna[x][y]=contMina;
                }
-               System.out.print("[  "+interna[x][y]+"  ]");
            }//for de columnas
-       }//for de filas 
+           
+       }//for de filas
+       Externa=new String[fil][col];
+       for(int i=0;i<fil;i++){
+           for(int j=0;j<col;j++){
+                Externa[i][j]="*";
+           }
+       }
+       Juego();
     }// metodo de llenado
+    public void Juego(){
+       System.out.println("\n\n\n");
+       System.out.println("               Elija una opcion");
+       for(int i=0;i<fil;i++){
+           System.out.println("");
+           for(int j=0;j<col;j++){
+                System.out.print("["+Externa[i][j]+"]");
+           }//for col
+       }//for fil
+       System.out.println("\n\n");
+       System.out.println("              1. Descubrir Casilla");
+       System.out.println("              2. Reinciar");
+       System.out.println("              3. Salir");
+       opJuego=scan.nextInt();
+       switch(opJuego){
+           case 1:
+               System.out.println("          Ingrese el numero de fila entre 1 y "+fil);
+               selFil=scan.nextInt();
+               selFil--;
+               System.out.println("          Ingrese el numero de columna entre 1 y "+col);
+               selCol=scan.nextInt();
+               selCol--;
+               if(interna[selFil][selCol]==-1){
+                   System.out.println("\n\n\n         Has perdido el juego por seleccionar una mina, vuelve a intentarlo.");
+                   llenadoMatriz();
+               }else{
+                   Externa[selFil][selCol]=Integer.toString(interna[selFil][selCol]);
+                   for(int v=0;v<4;v++){
+                       try{
+                       switch(v){
+                           case 0:
+                               if(interna[selFil-1][selCol]!=-1){
+                                   Externa[selFil-1][selCol]=Integer.toString(interna[selFil-1][selCol]);
+                               }
+                               break;
+                           case 1:
+                               if(interna[selFil][selCol+1]!=-1){
+                                   Externa[selFil][selCol+1]=Integer.toString(interna[selFil][selCol+1]);
+                               }
+                               break;
+                           case 2:
+                               if(interna[selFil+1][selCol]!=-1){
+                                   Externa[selFil+1][selCol]=Integer.toString(interna[selFil+1][selCol]);
+                               }
+                               break;
+                           case 3:
+                               if(interna[selFil][selCol-1]!=-1){
+                                   Externa[selFil][selCol-1]=Integer.toString(interna[selFil][selCol-1]);
+                               }
+                               break;
+                       }//switch
+                       }catch(Exception a){}//try
+                   }//for
+                   Juego();
+               }
+               
+               break;
+           case 2:
+               Menu m=new Menu();
+               m.menu();
+               break;
+           case 3:
+               System.exit(0);
+               break;
+           case 100:
+               for(int a=0;a<fil;a++){
+                   System.out.println("");
+                 for(int b=0;b<col;b++){
+                   System.out.print("[ "+interna[a][b]+" ]");
+               } }
+               Juego();
+               break;
+       }
+       System.out.println("\n\n");
+    }// metodo juego
+    
 }//clase matriz
